6a42ddb0799d2c636075085128e24a82d8ae26e5
DrFreud1 <santiagopozoruiz@gmail.com>
2022-11-23 00:19:15 -0500

    test: correct assertion in payment_manager testing

--- tests/test_payment_manager.py
+++ tests/test_payment_manager.py
@@ -7,4 +7,4 @@ def test_calculate_compensation():
     assert PaymentManager.calculate_compensation("MO", (time(9, 0), time(12, 0))) == 45
     assert PaymentManager.calculate_compensation("TH", (time(1, 0), time(3, 0))) == 50
     assert PaymentManager.calculate_compensation("SA", (time(14, 0), time(18, 0))) == 80
-    assert PaymentManager.calculate_compensation("SU", (time(20, 0), time(21, 0)))
+    assert PaymentManager.calculate_compensation("SU", (time(20, 0), time(21, 0))) == 25
